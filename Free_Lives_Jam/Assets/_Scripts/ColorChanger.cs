4d43125e623751b4c8837e7466f98cc6665df514
David <dlweatherhead@gmail.com>
2016-07-09 18:09:41 +0200

    Added Colour picking to change game object colour. Added hotkey for bringing up colour picker.

---
+++ Free_Lives_Jam/Assets/_Scripts/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+
+	// Use this for initialization
+	public void RegisterColorPicker () 
+	{
+		ColorPicker picker = GameObject.FindGameObjectWithTag("ColorPicker").GetComponent<ColorPicker>() as ColorPicker;
+
+		Renderer renderer = gameObject.GetComponent<Renderer>();
+
+		picker.onValueChanged.AddListener(color =>
+			{
+				renderer.material.color = color;
+			});
+		renderer.material.color = picker.CurrentColor;
+	}
+
+	public void DeregisterColorPicker() {
+		ColorPicker picker = GameObject.FindGameObjectWithTag("ColorPicker").GetComponent<ColorPicker>() as ColorPicker;
+
+		picker.onValueChanged.RemoveAllListeners();
+	}
+
+	void OnMouseDown() {
+		DeregisterColorPicker(); // Deregister all other color listeners.
+		RegisterColorPicker(); // Register this object to color picker
+	}
+}
---
+++ Free_Lives_Jam/Assets/_Scripts/ColorPickerActivator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorPickerActivator : MonoBehaviour {
+
+	private bool isActive = false;
+	private GameObject colorPicker;
+
+
+	// Use this for initialization
+	void Start () {
+		colorPicker = GameObject.FindGameObjectWithTag("ColorPicker");
+		colorPicker.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.C)) {
+			if(isActive) {
+				isActive = false;
+				colorPicker.SetActive(false);
+				ColorPicker picker = colorPicker.GetComponent<ColorPicker>() as ColorPicker;
+				picker.onValueChanged.RemoveAllListeners();
+			} else {
+				isActive = true;
+				colorPicker.SetActive(true);
+			}
+		}
+	}
+}
